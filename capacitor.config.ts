a33f8d4ed1775efc86c0144808a5d25cab2b5631
victorDeJ <vicalex2108@gmail.com>
2026-01-25 11:39:58 -0400

    update capacitor plugins

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cuentamimercado.app',
   appName: 'cuenta-mi-mercado',
-  webDir: 'dist/cuenta-mi-mercado/browser'
+  webDir: 'dist/cuenta-mi-mercado/browser',
+  plugins: {
+    StatusBar: {
+      overlay: false,
+    },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: '#CAD1C5',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
