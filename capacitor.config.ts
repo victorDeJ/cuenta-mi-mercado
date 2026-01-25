86ddac97dae8fdbdb1e0f7a62cd33c78b94eaad0
victorDeJ <vicalex2108@gmail.com>
2026-01-25 11:09:44 -0400

    update config files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cuentamimercado.app',
+  appName: 'cuenta-mi-mercado',
+  webDir: 'dist/cuenta-mi-mercado/browser'
+};
+
+export default config;
